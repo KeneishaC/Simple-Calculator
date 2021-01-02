b8fdcd63c371623594e2ae6be699071384d25c93
keneishac <keneishacorbett@gmail.com>
2021-01-01 22:49:49 -0800

    finishes calculator

--- js/script.js
+++ js/script.js
@@ -1,30 +1,47 @@
+// IIFE
 // IPO - Input Process Output 
+(function() {
+    //variables (state) and constants
+    let input, output
 
-//variables (state) and constants
-let input, output
+    //Cached Element References
+    const buttonCtnr = document.getElementById('buttons');
+    const clearBtnEl = document.getElementById('clear')
+    const inputEl = document.querySelector('input')
 
-//Cached Element References
-const buttonCtnr = document.getElementById('buttons');
-const clearBtnEl = document.getElementById('clear')
-const inputEl = document.querySelector('input')
+    //Event Listeners 
+    buttonCtnr.addEventListener('click', handleClick)
+    clearBtnEl.addEventListener('click', handleClearOrInit)
 
-//Event Listeners 
-buttonCtnr.addEventListener('click', handleClick)
-clearBtnEl.addEventListener('click', handleClearOrInit)
+    //Functions 
+    handleClearOrInit()
 
-//Functions 
-handleClearOrInit()
+    function handleClearOrInit() {
+        output = ""
+        render()
+    }
 
-function handleClearOrInit() {
-    output = ""
-    
-}
+    function handleClick(event){
+        input = event.target.classList.contains('button') 
+        ? event.target.dataset.value
+        : null
+        if(!input) return
 
-function handleClick(event){
-    input = event.target.classList.contains('button') 
-    ? event.target.dataset.value
-    : null
-    if(!input) return
-    
-    alert('Button clicked')
-}
+        if(input === "=") return calculate()
+
+        output += input
+        //render changes to the DOM
+        render()
+    }
+
+    function calculate() {
+        const calculation = new Function(`return (${output})`)
+        output = String(calculation())
+        render()
+    }
+
+    function render() {
+        if(output) inputEl.value = output
+        else inputEl.value = "0"
+    }
+})()
